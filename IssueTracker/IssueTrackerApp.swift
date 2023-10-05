b7d937b658b7d86efd1b143e910b257c44933b22
Chon Torres <chon@chontorres.com>
2023-10-05 14:01:38 -0700

    first commit

---
+++ IssueTracker/IssueTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IssueTrackerApp.swift
+//  IssueTracker
+//
+//  Created by Chon Torres on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct IssueTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
