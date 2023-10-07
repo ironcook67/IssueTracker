0b21cee877ba94f04d64b03ca65c18354a5e44ac
Chon Torres <chon@chontorres.com>
2023-10-07 12:29:09 -0700

    Added awardds calculation and button.

--- IssueTracker/DataManager/DataManager.swift
+++ IssueTracker/DataManager/DataManager.swift
@@ -203,6 +203,41 @@ class DataManager: ObservableObject {
         save()
     }
 }
+
+extension DataManager {
+    // MARK: - Awards
+    func count<T>(for fetchRequest: NSFetchRequest<T>) -> Int {
+        (try? container.viewContext.count(for: fetchRequest)) ?? 0
+    }
+    
+    func hasEarned(award: Award) -> Bool {
+        switch award.criterion {
+        case "issues":
+            // return true if they added a certain nnumber of issues
+            let fetchRequest = Issue.fetchRequest()
+            let awardCount = count(for: fetchRequest)
+            return awardCount >= award.value
+            
+        case "closed":
+            // return true if they closed a certain number of issues
+            let fetchRequest = Issue.fetchRequest()
+            fetchRequest.predicate = NSPredicate(format: "completed == true")
+            let awardCount = count(for: fetchRequest)
+            return awardCount >= award.value
+        
+        case "tags":
+            // return true if they created a certain number of tags
+            let fetchRequest = Issue.fetchRequest()
+            let awardCount = count(for: fetchRequest)
+            return awardCount >= award.value
+            
+        default:
+            // unknown award criterion. This should never happen.
+//            fatalError("Unknown award criterion \(award.criterion)")
+            return false
+        }
+    }
+}
     
 extension DataManager {
     func createSampleData() {
