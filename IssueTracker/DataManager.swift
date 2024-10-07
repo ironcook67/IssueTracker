946774dd5e5f7bfde359f191157c966e483e31b3
Chon Torres <chon@chontorres.com>
2024-10-06 20:25:57 -0700

    adusted testing evironment

--- IssueTracker/DataManager.swift
+++ IssueTracker/DataManager.swift
@@ -153,16 +153,7 @@ class DataManager: ObservableObject {
                 #endif // !os(watchOS)
             }
 
-#if DEBUG
-            // Used for UI Testing. Delete all of the data to start with a clean slate
-            // and turn off aninmation for faster tests.
-            if CommandLine.arguments.contains("enable-testing") {
-                self?.deleteAll()
-#if os(iOS)
-                UIView.setAnimationsEnabled(false)
-#endif // os(iOS)
-            }
-#endif  // DEBUG
+            self?.checkForTestingEvironment()
         }
     }
 
